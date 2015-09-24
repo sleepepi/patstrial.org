b4945768c5b53766563d71bf22650c29218c612c
Remo Mueller <remosm@gmail.com>
2015-09-24 10:52:50 -0400

    Started work on initial color scheme, font family, and user interface

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
 ## 1.0.0
 
 - Skeleton files to initialize Rails application with testing framework and continuous integration
+- Started work on initial color scheme, font family, and user interface
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+# Main web application controller for PATS Trial website
+# Other controllers inherit from this as a base class
+# This controller also handles several static pages in views/application
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, only: [:dashboard]
@@ -5,11 +8,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
   protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit :first_name, :last_name, :email,
+               :password, :password_confirmation
     end
-
+  end
 end
