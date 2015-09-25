f33bc694bbde66d8601aaa239a4d27d21ed1c8ac
Remo Mueller <remosm@gmail.com>
2015-09-25 11:37:25 -0400

    Updated styling, layout, and logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-
   scope module: 'application' do
+    get :credits
     get :dashboard
+    get :theme
     get :version
     get :welcome
+    get :window
   end
 
-  devise_for :users, path_names: { sign_up: 'join', sign_in: 'login', sign_out: 'logout' }, path: ""
+  devise_for :users, path_names: { sign_up: 'join',
+                                   sign_in: 'login',
+                                   sign_out: 'logout' },
+                     path: ''
 
   root to: 'application#welcome'
-
 end
--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -1,28 +1,44 @@
 require 'test_helper'
 
+# Tests accessibility of public static pages on the website
 class ApplicationControllerTest < ActionController::TestCase
-  test "should get dashboard for regular user" do
+  test 'should get credits' do
+    get :credits
+    assert_response :success
+  end
+
+  test 'should get dashboard for regular user' do
     login(users(:regular))
     get :dashboard
     assert_response :success
   end
 
-  test "should not get dashboard for logged out user" do
+  test 'should not get dashboard for logged out user' do
     get :dashboard
     assert_redirected_to new_user_session_path
   end
 
-  test "should get welcome" do
+  test 'should get theme' do
+    get :theme
+    assert_response :success
+  end
+
+  test 'should get welcome' do
     get :welcome
     assert_response :success
   end
 
-  test "should get version" do
+  test 'should get window' do
+    get :window
+    assert_response :success
+  end
+
+  test 'should get version' do
     get :version
     assert_response :success
   end
 
-  test "should get version as json" do
+  test 'should get version as json' do
     get :version, format: 'json'
     version = JSON.parse(response.body)
     assert_equal PatstrialOrg::VERSION::STRING, version['version']['string']
