5ec122f31f0aaec869bdedd66d0f77a593b77ba6
Remo Mueller <remosm@gmail.com>
2015-11-04 15:17:45 -0500

    Added abbreviation helpers to home page, closes #23, and addresses #17.

--- app/helpers/external_helper.rb
+++ app/helpers/external_helper.rb
@@ -1,2 +1,19 @@
 module ExternalHelper
+  def abbr(abbreviation)
+    content_tag :abbr, title: abbreviations[abbreviation][:name] do
+      abbreviation
+    end
+  rescue
+    abbreviation
+  end
+
+  def abbreviations
+    @abbreviations ||= load_yaml('abbreviations')
+  end
+
+  private
+
+  def load_yaml(name)
+    YAML.load_file(Rails.root.join('lib', 'data', "#{name}.yml"))
+  end
 end
