422ae715873184eeaa224d6aba6c708deeebab6b
Remo Mueller <remosm@gmail.com>
2018-03-14 13:20:43 -0400

    Version bump to 12.0.0.pre

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -2,11 +2,11 @@
 
 module PatstrialOrg
   module VERSION #:nodoc:
-    MAJOR = 11
+    MAJOR = 12
     MINOR = 0
     TINY = 0
-    BUILD = nil # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
+    BUILD = "pre" # "pre", "beta1", "beta2", "rc", "rc2", nil
 
-    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
+    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join(".")
   end
 end
