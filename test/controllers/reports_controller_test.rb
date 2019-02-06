35573282f93cb09a9e276c1a7c002f2561639c43
Remo Mueller <remosm@gmail.com>
2019-02-06 13:15:10 -0500

    Update test to integration test

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -1,82 +1,82 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 # Tests to assure that reports load correctly.
-class ReportsControllerTest < ActionController::TestCase
+class ReportsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @viewer = users(:viewer)
   end
 
-  test 'should get data quality' do
+  test "should get data quality" do
     login(@viewer)
-    get :data_quality
+    get reports_data_quality_url
     assert_response :success
   end
 
-  test 'should get screened' do
+  test "should get screened" do
     login(@viewer)
-    get :screened
+    get reports_screened_url
     assert_response :success
   end
 
-  test 'should get consented' do
+  test "should get consented" do
     login(@viewer)
-    get :consented
+    get reports_consented_url
     assert_response :success
   end
 
-  test 'should get eligible' do
+  test "should get eligible" do
     login(@viewer)
-    get :eligible
+    get reports_eligible_url
     assert_response :success
   end
 
-  test 'should get randomized' do
+  test "should get randomized" do
     login(@viewer)
-    get :randomized
+    get reports_randomized_url
     assert_response :success
   end
 
-  test 'should get demographics' do
+  test "should get demographics" do
     login(@viewer)
-    get :demographics
+    get reports_demographics_url
     assert_response :success
   end
 
-  test 'should get eligibility status' do
+  test "should get eligibility status" do
     login(@viewer)
-    get :eligibility_status
+    get reports_eligibility_status_url
     assert_redirected_to reports_eligibility_status_screened_path
   end
 
-  test 'should get eligibility status screened' do
+  test "should get eligibility status screened" do
     login(@viewer)
-    get :eligibility_status_screened
+    get reports_eligibility_status_screened_url
     assert_response :success
   end
 
-  test 'should get eligibility status consented' do
+  test "should get eligibility status consented" do
     login(@viewer)
-    get :eligibility_status_consented
+    get reports_eligibility_status_consented_url
     assert_response :success
   end
 
-  test 'should get grades' do
+  test "should get grades" do
     login(@viewer)
-    get :grades
+    get reports_grades_url
     assert_response :success
   end
 
-  test 'should get unscheduled events' do
+  test "should get unscheduled events" do
     login(@viewer)
-    get :unscheduled_events
+    get reports_unscheduled_events_url
     assert_response :success
   end
 
-  test 'should get failing checks' do
+  test "should get failing checks" do
     login(@viewer)
-    get :data_inconsistencies
+    get reports_data_inconsistencies_url
     assert_response :success
   end
 end
